332de1456e19b7bf28bb8d6eac731e79112c5d20
Usse <darkusse@gmail.com>
2014-12-02 09:04:52 +0000

    safeLog.js

---
+++ safeLog.js
@@ -0,0 +1,10 @@
+$.log = function() {
+	var consoleExists = (typeof(console) == "object") && (typeof(console.log) == "function");
+	if (consoleExists) {
+		if (navigator.userAgent.toLowerCase().indexOf("applewebkit") != -1) {
+			console.log(arguments);
+		} else {
+			console.log.apply(this,arguments);
+		}
+	}
+};
