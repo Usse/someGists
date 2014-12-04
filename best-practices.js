abe101299f2802775ac1d9651c66545f3d953d79
Usse <darkusse@gmail.com>
2014-12-04 11:01:12 +0000

    added best-practices.js

---
+++ best-practices.js
@@ -0,0 +1,27 @@
+var BP = BP || {};
+
+BP.Foo = function($) {
+  'use strict';
+
+  var self = {},
+  trigger,
+  element;
+
+  self.init = function(opts) {
+    trigger = opts.trigger;
+    element = opts.element;
+
+    trigger.click(function() {
+      self.FooBar();
+    });
+  }
+
+  self.FooBar = function() {
+    console.log('Foo!');
+  }
+
+  return {
+    init : self.init
+  }
+
+}
